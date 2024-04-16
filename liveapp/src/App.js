015dcaca605361e634beec337665b48c3fe002bc
Wesley Luiz de Souza <wesley_luiz2001@hotmail.com>
2024-04-16 13:23:30 +0000

    pqp

--- liveapp/src/App.js
+++ liveapp/src/App.js
@@ -4,7 +4,7 @@ import Streaming from './components/Streaming/Streaming';
 import Livechat from './components/livechat/Livechat';
 
 function App() {
-    const streamUrl = 'http://example.com/stream'; //actual streaming URL
+    const streamUrl = 'https://06c95fabdd6a.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.637423269549.channel.uoV4etCJXJtv.m3u8'; //actual streaming URL from amazon
 
     return (
         <div className="App">
--- liveapp/src/components/Streaming/Streaming.js
+++ liveapp/src/components/Streaming/Streaming.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-// Placeholder for the video stream
 function Streaming({ streamUrl }) {
     return (
-        <div className="streaming-container"> 
+        <div className="streaming-container">
             <video controls autoPlay={true} style={{ width: '100%' }}>
-                <source src={streamUrl} type="video/mp4" />
+                <source src={streamUrl} type="application/vnd.apple.mpegurl" />
                 Your browser does not support the video tag.
             </video>
         </div>
