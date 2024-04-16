0520ed43083d53c514079a90a78582f099a238a9
Wesley Luiz de Souza <wesley_luiz2001@hotmail.com>
2024-04-16 20:52:20 +0000

    managed to make the stream work with the hls library, the audio was looping for some reason

--- liveapp/src/components/Streaming/Streaming.js
+++ liveapp/src/components/Streaming/Streaming.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Hls from 'hls.js';
+
+// hls library is used to pass the stream onto the application
+// without hls the CORS policy blocks the request to the application.
 
 function Streaming({ streamUrl }) {
-    return (
-        <div className="streaming-container">
-            <video controls autoPlay={true} style={{ width: '100%' }}>
-                <source src={streamUrl} type="application/vnd.apple.mpegurl" />
-                Your browser does not support the video tag.
-            </video>
-        </div>
-    );
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        if (Hls.isSupported()) {
+            const hls = new Hls();
+            hls.loadSource(streamUrl);
+            hls.attachMedia(videoRef.current);
+            hls.on(Hls.Events.MANIFEST_PARSED, () => {
+                videoRef.current.play();
+            });
+
+            return () => hls.destroy();
+        } else if (videoRef.current.canPlayType('application/vnd.apple.mpegurl')) {
+            videoRef.current.src = streamUrl;
+            videoRef.current.addEventListener('loadedmetadata', () => {
+                videoRef.current.play();
+            });
+        }
+    }, [streamUrl]);
+
+    return <video ref={videoRef} controls style={{ width: '100%' }} />;
 }
 
 export default Streaming;
+
